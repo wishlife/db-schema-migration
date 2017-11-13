a13153b89d96a944a86671083460bc821ef2d831
Lei Wang <tendant@gmail.com>
2017-11-13 15:38:46 -0800

    Add schema migration

---
+++ src/db_schema_migration/schema.clj
@@ -0,0 +1,170 @@
+(ns db-schema-migration.schema
+  (:require [db-schema-migration.simple-jdbc :refer [execute]]
+            [db-schema-migration.schema-validate :refer [validate-schema]])
+  (:import (java.sql Connection
+                     DriverManager
+                     PreparedStatement
+                     ResultSet
+                     Statement))
+  (:gen-class))
+
+;; This is the basic record format of an upgrade
+;;
+;; The version-name must be a unique keyword from all other upgrades
+;;
+;; The upgrade and downgrade fields are either a SQL string or a
+;; function to run when performing the upgrade/downgrade process.
+;; When it is a function, it will recieve a single argument of a
+;; java.sql.Connection to use for the upgrade.  The connection will
+;; have auto-commit turned off, and the result will be committed after
+;; normal return from the function.
+;;
+;; When possible, it is highly preferred to have the upgrade and
+;; downgrade be SQL strings.  This is because the SQL will then be
+;; recorded in the schema_version table.
+(defrecord Migration [version-name upgrade downgrade])
+
+(defn create-table
+  "Helper function to make a 'CREATE TABLE' migration.  The upgrade
+  SQL is a 'CREATE TABLE <table-name> ( <defs joined on ','> ).  The
+  downgrade SQL is a 'DROP TABLE IF EXISTS'"
+  [vname table-name defs]
+  (Migration. vname
+              (format "CREATE TABLE %s (\n%s\n)"
+                      table-name
+                      (clojure.string/join ",\n" defs))
+              (str "DROP TABLE IF EXISTS " table-name)))
+
+(defn create-view
+  "Helper function to make a 'CREATE VIEW' migration.  This can save a
+  little typing."
+  [vname view query]
+  (Migration. vname
+              (str "CREATE VIEW " view " AS " query)
+              (str "DROP VIEW IF EXISTS " view)))
+
+(defn create-enum-table
+  "Helper function to create a table that acts a an enum."
+  [vname enum-name names]
+  (Migration. vname
+              ;; We use VARCHAR(63) for enum names as that postgres's
+              ;; limit on CREATE TYPE [...] ENUM names.
+              (str "CREATE TABLE " enum-name " (name VARCHAR(63) PRIMARY KEY);\n"
+                   (when (not (empty? names))
+                     (str "INSERT INTO " enum-name " VALUES "
+                          (->> (map #(str "('" % "')") names)
+                               (clojure.string/join ",")))))
+              (str "DROP TABLE IF EXISTS " enum-name)))
+
+(defn insert-enum-table
+  "Helper function to add new values to an enum."
+  [vname enum-name names]
+  (Migration. vname
+              (str "INSERT INTO " enum-name " VALUES "
+                   (->> (map #(str "('" % "')") names)
+                        (clojure.string/join ",")))
+              ""))
+
+(defn- database-name
+  [db-subname]
+  (clojure.string/replace db-subname #"^.*/|\?.*$" ""))
+
+(defn- open-connection
+  [classname subprotocol subname user password]
+  (let [url (format "jdbc:%s:%s" subprotocol subname)]
+    (Class/forName classname)
+    (try
+      (DriverManager/getConnection url user password)
+      (catch Exception ex
+        (let [database (database-name subname)]
+          (printf "Could not open connection to %s.%n" url)
+          (printf "Please verify database exists and that %s has access to it.%n" user)
+          (printf "If this is a new database, please run the following command in psql%n")
+          (printf "CREATE USER %s WITH PASSWORD '%s';%n" user password)
+          (printf "CREATE DATABASE %s ENCODING 'UTF8' OWNER %s;%n" database user)
+          (printf "GRANT ALL PRIVILEGES ON DATABASE %s TO %s;%n" database user))
+        (throw ex)))))
+
+(defn- run-migrations
+  [^Connection conn action migrations]
+  (let [count-to-run (count migrations)]
+    (try
+      (loop [no 1 migrations migrations]
+        (if-let [migration (first migrations)]
+          (let [ver (name (:version-name migration))
+                process (get migration action)]
+            (printf "Running %s for %s (%d of %d)..." (name action) ver no count-to-run)
+            (flush)
+            (condp #(%1 %2) process
+              string? (execute conn process)
+              fn? (process conn)
+              (throw (IllegalArgumentException. (str "don't know how to run " ver))))
+            (-> (case action
+                  :upgrade (execute conn "INSERT INTO schema_version (version_name,sql) VALUES (?,?)" [ver (str process)])
+                  :downgrade (execute conn "DELETE FROM schema_version WHERE version_name = ?" [ver]))
+                (= 1) (or (throw (IllegalStateException. "version not bumped"))))
+            (.commit conn)
+            (println " done.")
+            (recur (inc no) (next migrations)))
+          (println (if (= 1 no) "No migrations to run." "Migrations complete."))))
+      (catch Exception ex
+        (printf " failed (%s)%n" (.getMessage ex))
+        (.rollback conn)
+        (throw ex)))))
+
+(defn- migrate-schema
+  [migration-levels action] ;; TODO: add support for additional parameter [target-version]
+  {:pre [(contains? #{:upgrade :downgrade} action)]}
+  (println "Starting" (name action))
+  (with-open [^Connection conn (open-connection)]
+    ;; Disable sanity check, no need any more
+    ;; (migration-sanity-check conn)
+    (.setAutoCommit conn false)
+    (println "Creating schema_version table if it does not exist")
+    (execute conn "CREATE TABLE IF NOT EXISTS schema_version (
+                     version_name VARCHAR(80) NOT NULL UNIQUE,
+                     migrated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+                     sql TEXT NOT NULL
+                  )")
+    (.commit conn)
+    (let [version-set (->> (execute conn "SELECT version_name FROM schema_version")
+                           (map :version-name)
+                           (map keyword)
+                           (into #{}))
+          migration-levels (load "migration-levels")]
+      (printf "Schema currently has %d of %d upgrades%n" (count version-set) (count migration-levels))
+      (case action
+        :upgrade
+        (->> migration-levels
+             (remove #(contains? version-set (:version-name %)))
+             (run-migrations conn :upgrade))
+
+        :downgrade
+        ;; downgrades would work something like this:
+        ;; BUT MAKE SURE target-version EXISTS, or it will downgrade to an empty database!
+        ;; (->> (reverse migration-levels)
+        ;;      (take-while #(not= target-version (:version-name %)))
+        ;;      (filter #(contains? version-set (:version-name %)))
+        ;;      (run-migrations conn :downgrade))
+        (throw (UnsupportedOperationException. "downgrade is not currently supported"))))))
+
+(defn -main
+  [& args]
+  (-> (try
+        (if (next args)
+          (throw (IllegalArgumentException. "Additional command arguments are not supported"))
+          (case (first args)
+            nil (println "Please specify one of 'upgrade', 'downgrade', or 'validate'")
+            "validate" (validate-schema (database-name))
+            ("upgrade" "downgrade") (migrate-schema (keyword (first args)))
+            (throw (UnsupportedOperationException. (str "Invalid command: " (first args))))))
+        (catch Throwable ex
+          (.printStackTrace ex)
+          2) ;; exit code
+        (finally
+          ;; validate uses agents to read sub-process streams.  we
+          ;; need to shut them down.
+          (shutdown-agents)
+          (flush)))
+      (or 0) ;; convert nil to exit code 0
+      (System/exit)))
---
+++ src/db_schema_migration/schema_validate.clj
@@ -0,0 +1,63 @@
+(ns db-schema-migration.schema-validate
+  (:import (java.io File)
+           (java.net URL)
+           (java.text SimpleDateFormat)
+           (java.util Date))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn- exec
+  ([cmd-array]
+   (exec cmd-array nil))
+  ([cmd-array input]
+   (let [^Process proc (.exec (Runtime/getRuntime) (into-array String cmd-array))]
+     (if input
+       (send (agent nil) (fn [_] (with-open [os (.getOutputStream proc)]
+                                   (spit os input))))
+       (.. proc getOutputStream close))
+     (let [out (send (agent nil) #(slurp %2) (.getInputStream proc))
+           err (send (agent nil) #(slurp %2) (.getErrorStream proc))
+           exit (.waitFor proc)]
+       (await out err)
+       {:exit exit :out @out :err @err}))))
+
+(defn- cleanup-dump
+  [dump]
+  ;; the follow could probably be made more efficient by using fewer
+  ;; string/replaces, but this is hardly the slowest thin in the
+  ;; process.
+  (-> dump
+      (str/replace #"(?m)^--.*$" "") ;; remove comments
+      (str/replace #"(?m)^ALTER TABLE.*OWNER TO.*;$" "") ;; remove table ownership
+      (str/replace #"(?m)^(?:REVOKE|GRANT)\s.*;$" "") ;; remove grants/revokes
+      (str/replace #"(?<=^|\r\n?|\n)(?:\r\n?|\n)+" ""))) ;; remove empty lines
+
+(defn- compare-dump
+  [expected actual]
+  (if-not (and (instance? URL expected)
+               (= "file" (.getProtocol expected)))
+    (throw (IllegalArgumentException. (format "cannot diff '%s'" expected))))
+  (let [diff (exec ["diff" (.getFile expected) "-"] actual)]
+    (case (:exit diff)
+      0 (println "Schema is valid")
+      1 (let [dir (.getParentFile (File. (.getFile expected)))
+              file (File. dir (.format (SimpleDateFormat. "'schema-validate.'YYYY-MM-dd'T'HHmmss'.sql'") (Date.)))]
+          (println "Schema differences found!")
+          (println (:out diff))
+          (println "Writing current schema to" (.getAbsolutePath file))
+          (println "If the above differences are correct, please copy this file over schema-validate.sql")
+          (if (.exists file)
+            (printf "WARNING %s exists! Not overwriting.%n" file)
+            (spit file actual)))
+      (printf "DIFF ERROR! (code = %d)%n%s%n" (:exit diff) (or (:err diff)
+                                                               (:out diff))))
+    (:exit diff)))
+
+(defn validate-schema
+  [database-name]
+  {:pre [(re-matches #"^\w+$" database-name)]}
+  (let [dump (exec ["pg_dump" "--schema-only" database-name])]
+    (if-not (= 0 (:exit dump))
+      (printf "validation failed: %s%n" (clojure.string/trim (:err dump)))
+      (compare-dump (io/resource "schema-validate.sql")
+                    (cleanup-dump (:out dump))))))
