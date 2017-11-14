74ba18cad2e7df5e222fcf43faf0fc0577e999ae
Lei Wang <tendant@gmail.com>
2017-11-13 16:55:46 -0800

    release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject db-schema-migration "0.1.0-SNAPSHOT"
+(defproject db-schema-migration "0.1.0"
   :description "A simple database schema migration tool for Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
