138be2ef5c0940c40a291102c0790e895bb8137e
Lei Wang <tendant@gmail.com>
2020-05-04 11:19:45 -0700

    add export-schema

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject db-schema-migration "0.1.6"
+(defproject db-schema-migration "0.1.7-SNAPSHOT"
   :description "A simple database schema migration tool for Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
